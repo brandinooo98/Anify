06d5838e97cdda7cd20fdad54bdc040e82adaabd
Brandon Erickson <brandon.erickson@willowtreeapps.com>
2025-03-27 14:21:44 -0400

    Remove old directories

--- python-backend-WIP/api/src/user/series.py
+++
@@ -1,9 +0,0 @@
-from database import db
-
-
-class Series(db.Model):
-    name = db.Column(db.String(64), unique=True, primary_key=True)
-    user = db.Column(db.String(64))
-
-    def __repr__(self):
-        return "<Series {0} for User {1}>".format(self.name, self.user)
--- python-backend-WIP/api/src/user/user.py
+++
@@ -1,8 +0,0 @@
-from database import db
-
-
-class User(db.Model):
-    username = db.Column(db.String(64), index=True, unique=True, primary_key=True)
-
-    def __repr__(self):
-        return "<User {}>".format(self.username)
